08e3f95a88edf97ba583b01b69559e27090dbdf9
Luthero Boscardin Vargas <luthero.b@edu.pucrs.br>
2025-08-13 21:44:14 +0000

    luthero criou mais tres tipos de codificação, além de fazer um switch para escolher a codificação

--- CodificaPrj/src/main/java/fds/klippel/App.java
+++ CodificaPrj/src/main/java/fds/klippel/App.java
@@ -1,5 +1,7 @@
 package fds.klippel;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Codificador cod = new CodificadorSimples();
@@ -7,12 +9,16 @@ public class App {
         System.out.println("Codificador: "+cod.getNome());
         System.out.println("Versao: "+cod.getDataCriacao());
         System.out.println("Nivel de segurança: "+cod.getNivelSeguranca());
-        
-        String texto = "Este e o string a ser codificado";
-        String codificado = cod.codifica(texto);
-        String decodificado = cod.decodifica(codificado);
 
-        System.out.println("Texto original: "+texto);
+        Scanner ent = new Scanner(System.in);
+        System.out.println("digite a palavra a ser codificada: ");
+        String entrada = ent.nextLine();
+        System.out.println("Qual a codificação que você quer?\n 1- +1 letra\n 2- +2 letras\n 3- +3 letras\n 4- *2 letras");
+        int opcao = ent.nextInt();
+        String codificado = cod.codifica(entrada, opcao);
+        String decodificado = cod.decodifica(codificado, opcao);
+
+        System.out.println("Texto original: "+entrada);
         System.out.println("Texto codificado: "+codificado);
         System.out.println("Texto decodificado: "+decodificado);
     }

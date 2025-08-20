654250e1576accdd05703313d46b50880e45b6ea
FKlippel <laura.klippel@edu.pucrs.br>
2025-08-20 21:25:13 +0000

    Codificador Luthero adicionado

--- CodificaPrj/src/main/java/fds/klippel/App.java
+++ CodificaPrj/src/main/java/fds/klippel/App.java
@@ -5,9 +5,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         Codificador cod1 = new Codificador_Arthur();
         Codificador cod2 = new CodificadorSimples();
+        Codificador cod3 = new Codificador_Luthero();
+
 
         Factory.codificadores.add(cod1);
         Factory.codificadores.add(cod2);
+        Factory.codificadores.add(cod3);
 
         Scanner s = new Scanner(System.in);
 
